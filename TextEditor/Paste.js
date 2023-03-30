b3b5fbdf37b281db5efa179dcc61b004d243c81a
GabrielRalph <gltralph@gmail.com>
2023-03-31 00:17:27 +1100

    print size better

--- TextEditor/Paste.js
+++ TextEditor/Paste.js
@@ -9,54 +9,63 @@ function sanitizePasteText(text) {
     } else {
       node = document.createDocumentFragment();
       for (let line of tlines) {
-        console.log(line);
+        // console.log(line);
         if (node.childNodes.length > 0) {
           node.append(document.createElement('br'));
         }
-        node.append(document.createTextNode(line));
+
+        let linetext = line.replace(/(?:^|\n)\s+/, (a) => {
+          let span = document.createElement("SPAN");
+          for (let i = 0; i < a.length; i++) {
+            span.innerHTML += "&nbsp;"
+            // console.log(a.charCodeAt(i));
+          }
+          node.append(span)
+          return ``
+        })
+        node.append(document.createTextNode(linetext));
       }
     }
   }
   return node;
 }
 
-function sanitizePasteHTML(html) {
-  var parser = new DOMParser();
-  let doc = parser.parseFromString(html, 'text/html');
-  let init = true;
-  let sanitizeNode = (node) => {
-    let items = MathJax.startup.document.getMathItemsWithin(node);
-    let sanitized = new Text(node.data);
-    // console.log(node);
-    if (!(node instanceof Text)) {
-      let children = [...node.childNodes]
-      if (node.tagName.toLowerCase() in AllowedTags) {
-        sanitized = document.createElement(node.tagName);
-        for (let styleKey in AllowedStyles) {
-          let value = AllowedStyles[styleKey](node.style[styleKey]);
-          if (value != null) {
-            sanitized.style.setProperty(styleKey, value);
-          }
-        }
-        for (let child of children) {
-          sanitized.appendChild(sanitizeNode(child));
-        }
-      } else {
-        sanitized = new DocumentFragment();
-        for (let child of children) {
-          sanitized.append(sanitizeNode(child));
-        }
-      }
-    }
-
-    return sanitized;
-  }
-
-  let body = doc.body;
-  let parsed = sanitizeNode(body);
-
-  return parsed;
-}
+//   let parser = new DOMParser();
+//   let doc = parser.parseFromString(html, 'text/html');
+//   let init = true;
+//   let sanitizeNode = (node) => {
+//     let items = MathJax.startup.document.getMathItemsWithin(node);
+//     let sanitized = new Text(node.data);
+//     // console.log(node);
+//     if (!(node instanceof Text)) {
+//       let children = [...node.childNodes]
+//       if (node.tagName.toLowerCase() in AllowedTags) {
+//         sanitized = document.createElement(node.tagName);
+//         for (let styleKey in AllowedStyles) {
+//           let value = AllowedStyles[styleKey](node.style[styleKey]);
+//           if (value != null) {
+//             sanitized.style.setProperty(styleKey, value);
+//           }
+//         }
+//         for (let child of children) {
+//           sanitized.appendChild(sanitizeNode(child));
+//         }
+//       } else {
+//         sanitized = new DocumentFragment();
+//         for (let child of children) {
+//           sanitized.append(sanitizeNode(child));
+//         }
+//       }
+//     }
+//
+//     return sanitized;
+//   }
+//
+//   let body = doc.body;
+//   let parsed = sanitizeNode(body);
+//
+//   return parsed;
+// }
 
 export function handlePaste(e){
   let element = null;
@@ -64,13 +73,13 @@ export function handlePaste(e){
   // check for clip board data
   let clip = e.clipboardData || window.clipboardData;
   if (clip) {
-    let html = clip.getData("text/html");
-    if (typeof html === "string" && html.length > 0) {
-      element = sanitizePasteHTML(html);
-    } else {
-      let text = clip.getData("text/plain");
+    // let html = clip.getData("text/html");
+    let text = clip.getData("text/plain");
+    // if (typeof html === "string" && html.length > 0) {
+      // element = sanitizePasteHTML(html);
+    // } else {
       element = sanitizePasteText(text);
-    }
+    // }
   }
 
   e.preventDefault();
